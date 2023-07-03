8492b861d2ca1152314992788ea9211cf6b8e348
Mohamed-Tawfek <mohamedtawfek.eng@gmail.com>
2023-07-04 01:02:47 +0300

    add icon &  splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:fast_drugs/helpers/cash_helper.dart';
 import 'package:fast_drugs/helpers/dio_helper.dart';
 import 'package:fast_drugs/shared/constants/api_constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'app.dart';
@@ -9,6 +10,7 @@ import 'controller/bloc_observer.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+
 await DioHelper.init();
 await CashHelper.init();
 //CashHelper.remove(key: 'userID');
